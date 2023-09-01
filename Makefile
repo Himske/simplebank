b61ffa9aceb0283f1784a9fb28d239248cfb9918
Fredrik Nilsson <himske@gmail.com>
2023-09-01 08:23:36 +0200

    Add gRPC API

--- Makefile
+++ Makefile
@@ -45,4 +45,13 @@ server:
 mock:
 	mockgen -package mockdb -destination db/mock/store.go github.com/himske/simplebank/db/sqlc Store
 
-.PHONY: network postgres createdb dropdb migrateup migrateup1 migratedown migratedown1 sqlc_init sqlc_generate test server mock db_docs db_schema
+proto:
+	del pb\*.go
+	protoc --proto_path=proto --go_out=pb --go_opt=paths=source_relative \
+    --go-grpc_out=pb --go-grpc_opt=paths=source_relative \
+    proto/*.proto
+
+evans:
+	evans --host localhost --port 9090 -r repl
+
+.PHONY: network postgres createdb dropdb migrateup migrateup1 migratedown migratedown1 sqlc_init sqlc_generate test server mock db_docs db_schema proto evans
---
+++ gapi/server.go
@@ -0,0 +1,33 @@
+package gapi
+
+import (
+	"fmt"
+
+	db "github.com/himske/simplebank/db/sqlc"
+	"github.com/himske/simplebank/pb"
+	"github.com/himske/simplebank/token"
+	"github.com/himske/simplebank/util"
+)
+
+// Server serves gRPC requests for our banking service.
+type Server struct {
+	pb.UnimplementedSimpleBankServer
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+}
+
+// NewServer creates a new gRPC server.
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("connot create token maker: %w", err)
+	}
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
+
+	return server, nil
+}
