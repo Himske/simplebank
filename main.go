24541455879a79619935e5cb9d8cdf2d093672b2
Fredrik Nilsson <himske@gmail.com>
2023-09-04 10:09:19 +0200

    Embedd front-end code

--- main.go
+++ main.go
@@ -10,10 +10,12 @@ import (
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/himske/simplebank/api"
 	db "github.com/himske/simplebank/db/sqlc"
+	_ "github.com/himske/simplebank/doc/statik"
 	"github.com/himske/simplebank/gapi"
 	"github.com/himske/simplebank/pb"
 	"github.com/himske/simplebank/util"
 	_ "github.com/lib/pq"
+	"github.com/rakyll/statik/fs"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/encoding/protojson"
@@ -86,8 +88,13 @@ func runGatewayServer(config util.Config, store db.Store) {
 	mux := http.NewServeMux()
 	mux.Handle("/", grpcMux)
 
-	fs := http.FileServer(http.Dir("./doc/swagger"))
-	mux.Handle("/swagger/", http.StripPrefix("/swagger/", fs))
+	statikFS, err := fs.New()
+	if err != nil {
+		log.Fatal("cannot create statik fs:", err)
+	}
+
+	swaggerHandler := http.StripPrefix("/swagger/", http.FileServer(statikFS))
+	mux.Handle("/swagger/", swaggerHandler)
 
 	listener, err := net.Listen("tcp", config.HTTPServerAddress)
 	if err != nil {
